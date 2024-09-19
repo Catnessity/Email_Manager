ef89fc2c3c86f98ae01707921a04b043653d54b5
Catnessity <xlisax94@gmail.com>
2024-09-19 10:48:43 +0200

    update program.cs with outlook imap

--- Code Snippets/MailTest/Program.cs
+++ Code Snippets/MailTest/Program.cs
@@ -6,53 +6,72 @@ using MailKit.Net.Imap;
 using Org.BouncyCastle.Bcpg;
 using Org.BouncyCastle.Asn1.X509;
 using System.Security.Cryptography;
+using System.ComponentModel.DataAnnotations;
 internal class Program
 {
+    
     private static void Main(string[] args)
     {
+        
         using (var client = new ImapClient())
         {
-            Console.WriteLine("Use private password?");
-            string choice = Console.ReadLine();
-            string password = "";
+            //Console.WriteLine("Use private password?");
+            //string choice = Console.ReadLine();
+            //string password = "";
 
-            if (choice.ToLower() == "y" || choice.ToLower() == "yes")
+            //if (choice.ToLower() == "y" || choice.ToLower() == "yes")
+            //{
+            //    System.Console.Write("password: ");
+            //    while (true)
+            //    {
+            //        ConsoleKeyInfo consoleKeyInfo = System.Console.ReadKey();
+            //        if (consoleKeyInfo.Key == ConsoleKey.Enter)
+            //        {
+            //            break;
+            //        }
+            //        password += consoleKeyInfo.KeyChar;
+            //    }
+            //}
+            //else
+            //{
+                var password = "C-mailT0123!";
+            //}
+            Console.WriteLine("Please choose the email domain you want to use:");
+            Console.WriteLine("1) GMX");
+            Console.WriteLine("2) Gmail");
+            Console.WriteLine("3) Outlook");
+            //Console.WriteLine("4) Protonmail"); //proton would need pro version to allow imap
+            var key = Console.ReadKey(true);
+            var imap_address = "";
+            var email = "";
+
+            switch (key.KeyChar)
             {
-                System.Console.Write("password: ");
-                while (true)
-                {
-                    var key = System.Console.ReadKey(true);
-                    if (key.Key == ConsoleKey.Enter)
-                        break;
-                    password += key.KeyChar;
-                }
+                case '1':
+                    imap_address = "imap.gmx.net";
+                    email = "c-mail.test@gmx.at";
+                    break;
+                case '2':
+                    imap_address = "imap.gmail.com";
+                    email = "c.mail.testmail@gmail.com";
+                    break;
+                case '3':
+                    imap_address = "imap-mail.outlook.com";
+                    email = "ctestlinz@outlook.com";
+                    break;
+                //case '4':
+                //    imap_address = "imap.protonmail.ch";
+                //    email = "c-mail.test@proton.me";
+                //    break;
+
+                default:
+                    throw new Exception("Please enter valid character");
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Please choose the email domain you want to use:");
-                Console.WriteLine("1) GMX");
-                Console.WriteLine("2) Gmail");
-                var key = Console.ReadKey(true);
+            client.Connect(imap_address, 993, true);
+            client.Authenticate(email, password);
 
-                switch (key.KeyChar)
-                {
-                    case '1':
-                        password = "C-mailT0123!";
-                        client.Connect("imap.gmx.net", 993, true);
-                        client.Authenticate("c-mail.test@gmx.at", password);
-                        break;
-                    case '2':
-                        password = "anfanjlunavnzctj";
-                        client.Connect("imap.gmail.com", 993, true);
-                        client.Authenticate("c.mail.testmail@gmail.com", password);
-                        break;
 
-                    default:
-                        throw new Exception("Please enter valid character");
-                        break;
-                }
-
-            }
 
 
 
