ed9df301fea8262adc1d5f06901dcc3cdefe0b35
Catnessity <xlisax94@gmail.com>
2024-09-25 14:06:16 +0200

    add color to missing loginvalues + support more email provider

--- PPNewsletterFilter/PPNewsletterFilter/PPNewsletterFilter/LoginView.xaml.cs
+++ PPNewsletterFilter/PPNewsletterFilter/PPNewsletterFilter/LoginView.xaml.cs
@@ -4,6 +4,8 @@ using System.Windows.Controls;
 using MailKit.Net.Imap;
 using MailKit;
 using System.Windows.Input;
+using System.Xml.Schema;
+using System.Windows.Media;
 
 namespace PPNewsletterFilter
 {
@@ -21,7 +23,7 @@ namespace PPNewsletterFilter
 
         private void txtEmail_TextChanged(object sender, TextChangedEventArgs e)
         {
-            
+
         }
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)
@@ -29,20 +31,66 @@ namespace PPNewsletterFilter
             string mail = txtEmail.Text;
             string password = pwdPassword.Password;
 
-            
+            //if email empty return with color
+            if (mail == "") {
+                Mail.Foreground = new SolidColorBrush(Colors.Red);
+            }else
+            {
+                Mail.Foreground = new SolidColorBrush(Colors.WhiteSmoke);
+            }
+            //if password empty return with color
+            if (password == "")
+            {
+                Password.Foreground = new SolidColorBrush(Colors.Red);
+                return;
+            }else
+            {
+                Password.Foreground = new SolidColorBrush(Colors.WhiteSmoke);
+            }
 
             // Connect to IMAP server and authenticate
             using (var client = new ImapClient())
             {
+                var imap_address = "";
+
+                //get the corret imap address based on the given email provider
+                if (mail.Contains("@gmx."))
+                {
+                    imap_address = "imap.gmx.net";
+                }
+                else if (mail.Contains("@gmail."))
+                {
+                    imap_address = "imap.gmail.com";
+                }
+                else if (mail.Contains("@outlook.") || password.Contains("@hotmail"))
+                {
+                    imap_address = "imap-mail.outlook.com";
+                }
+                else
+                {
+                    //MessageBox.Show($"Please check your email.", "Invalid Email", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    feedback.Text = "Please check your email, either there is a typo or your provider is not supported.";
+                    return;
+                }
+
+                //try to connect to the imap server with the given password
                 try
                 {
-                    client.Connect("imap.gmx.net", 993, true);
+                    client.Connect(imap_address, 993, true);
                     client.Authenticate(mail, password);
+                }
+                catch (Exception ex)
+                {
+                    //MessageBox.Show($"Please check your credentials.", "Login Failed", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    feedback.Text = "The connection to the server or the authentication failed. \n Check your password and try again.";
+                    return;
+                }
 
+                try
+                {
                     // The Inbox folder is always available on all IMAP servers...
                     var inbox = client.Inbox;
                     inbox.Open(FolderAccess.ReadWrite);
-
 
                     Dictionary<string, int> map = new Dictionary<string, int>();
 
@@ -68,7 +116,7 @@ namespace PPNewsletterFilter
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show($"Error: {ex.Message}", "Login Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show($"Error: {ex.Message}", "Something went wrong while processing the incoming messages", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
         }
