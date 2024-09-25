4e50736792d6dff41de666956dd01e78295a8213
Catnessity <xlisax94@gmail.com>
2024-09-25 14:02:40 +0200

    add server for download page + inital page

---
+++ server/public-html/js/game.js
@@ -0,0 +1,83 @@
+document.addEventListener('DOMContentLoaded', event =>{
+    setupGame();
+})
+
+async function setupGame(){
+    let gameboard = document.getElementById("DownloadGame");
+    for(i=0; i<=8; i++){
+        //add new row start
+        let row_content = '<div class="row gamerow"> ';
+        //add cols
+        let cols = Math.round(Math.random() * 6); //max ads per row
+        for(j=0; j<= cols; j++){
+            //add ads
+            row_content = await insertColContent(row_content);
+        }
+        //end row
+        row_content += '</div>';
+        gameboard.innerHTML += row_content;
+    }
+    setDownloadButton();
+
+}
+async function pathExists(path){
+    let res;
+    try {
+       res = await fetch("http://localhost:8080/" + path,
+            { method: "HEAD" }
+        );
+    } catch (error) {
+        console.log(error);
+        return false;
+    }   
+    //console.log(res);
+    if (res.ok){
+        console.log(path + "true");
+        return true;
+    }
+    else{
+        console.log(path + "false");
+        return false;
+    }
+}
+
+async function insertColContent(row_content){
+    let ad = Math.round(Math.random() * 18); //maximum ads
+     //check if path exists and in case get color instead
+    
+    //  pathExists('images/ads/ad' + ad + '.jpg').then((exists) => {
+    let result = await pathExists('images/ads/ad' + ad + '.jpg');
+    console.log("res: "+ result);
+    if (result){
+        console.log("exists");
+        //add image
+        //TODO size images
+        row_content += '<div class="col gamefield"> <img src="images/ads/ad' + ad + '.jpg">  </div>';
+        //row_content += '<div class="col gamefield" style="images/ads/ad' + ad + '.jpg">  </div>';
+        //row_content += '<div class="col gamefield" style="background-image: url("images/ads/ad' + ad + '.jpg"); background-size: cover; height: 400px; width: 100%;"></div>'
+    }
+    else{
+        console.log("does not exist");
+        let color = "#";
+        for(k=0; k < 6; k++){
+            let value = Math.round(Math.random() * 16);
+            if(value > 9){
+                //add enough so a 10 comes to 65 (asci A)
+                value += 55;
+                value = String.fromCharCode(value);
+            }
+            color += value;
+        }
+        row_content += '<div class="col gamefield" style="background-color:' +  color + ';">  </div>';
+    }
+    return row_content;
+}
+
+function setDownloadButton(){
+    let fields = document.getElementById("DownloadGame").getElementsByClassName('gamefield');
+    console.log(fields.length);
+    let button_field = Math.round(Math.random() * fields.length);
+    console.log("button: " + button_field);
+    fields[button_field].innerHTML += "<span> <button class='download-btn-light'> DOWNLOAD C-MAIL </button>";
+    fields[button_field].innerHTML += "<button class='download-btn-dark'> DOWNLOAD C-MAIL </button> </span>";
+}
